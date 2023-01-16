59fd9c73a8aa157bc95b3d75cd75ec1bd046092b
Matteo Ludwig <ludwig@silpion.de>
2023-01-16 15:50:36 +0100

    added delete swipe action to TaskList

--- teha/Tasks/TasksListView.swift
+++ teha/Tasks/TasksListView.swift
@@ -16,11 +16,49 @@ struct TasksListView: View {
 }
 
 fileprivate struct FilteredTasksListView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    
     @FetchRequest var tasks: FetchedResults<THTask>
+    
+    @State var taskToDelete: THTask?
+    
+    var showDeleteDialogBinding: Binding<Bool> {
+        Binding {
+            taskToDelete != nil
+        } set: { newVal, _ in
+            if !newVal {
+                taskToDelete = nil
+            }
+        }
+
+    }
     
     var body: some View {
         List(tasks) { task in
             TaskRowView(task: task)
+                .swipeActions(edge: .trailing) {
+                    Button() {
+                        taskToDelete = task
+                    } label: {
+                        Label("delete", systemImage: "minus.circle.fill")
+                    }
+                    .tint(Color.red)
+                }
+                .id(task.id)
+
+        }
+        .confirmationDialog("task-delete-confirmation", isPresented: showDeleteDialogBinding) {
+            Button("delete", role: .destructive) {
+                guard let taskToDelete = taskToDelete else { return }
+                viewContext.delete(taskToDelete)
+                // TODO: error handling
+                try? viewContext.save()
+            }
+            Button("cancel", role: .cancel) {
+                showDeleteDialogBinding.wrappedValue = false
+            }
+        } message: {
+            Text("task-delete-confirmation")
         }
     }
 }
