80e530bb53282c5449e62ae197d6e83a6d85eac3
margo1999 <jette.oberlaender@rwth-aachen.de>
2023-01-22 17:14:34 +0100

    Added Alert if user wants to set a reminder but did not allow the app to send notifications
    remaining bug: If the user allows notifications to be sent, the view will not be updated until TaskEditView is closed and reopened. However, the view should change as soon as the notificationStatus variable is set.

--- teha/Tasks/ReminderPicker.swift
+++ teha/Tasks/ReminderPicker.swift
@@ -7,27 +7,73 @@
 
 import SwiftUI
 
+class NotificationStatus: ObservableObject {
+    @Published var status: UNAuthorizationStatus = .notDetermined
+}
+
 struct ReminderPicker: View {
+    @ObservedObject var notificationStatus: NotificationStatus
     @Binding var internalSelection: ReminderOffsetTag
+    @State private var showAlert = false
     
     init(selection: Binding<ReminderOffset?>) {
-
+        
+        self.notificationStatus = NotificationStatus()
+        
         self._internalSelection = Binding {
             return ReminderOffsetTag(selection.wrappedValue)
         } set: { reminderOffsetTag in
             selection.wrappedValue = reminderOffsetTag.value
         }
+        
+        checkAuthorization()
+        
     }
     
-    var body: some View {
-        Picker("reminder-add", selection: $internalSelection) {
-            Text("none").tag(ReminderOffsetTag(nil))
-            
-            ForEach(ReminderOffset.allCases) { reminderOffset in
-                Text(reminderOffset.name).tag(ReminderOffsetTag(reminderOffset))
+    func checkAuthorization() {
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+            DispatchQueue.main.async {
+                self.notificationStatus.status = settings.authorizationStatus
             }
         }
     }
+
+
+    var body: some View {
+        Group {
+            
+            if notificationStatus.status == .authorized {
+                
+                Picker("reminder-add", selection: $internalSelection) {
+                    Text("none").tag(ReminderOffsetTag(nil))
+
+                    ForEach(ReminderOffset.allCases) { reminderOffset in
+                        Text(reminderOffset.name).tag(ReminderOffsetTag(reminderOffset))
+                    }
+                }
+                
+            } else {
+                
+                Button {
+                    self.showAlert.toggle()
+                } label: {
+                    Label("reminder-add", systemImage: "exclamationmark.triangle")
+                }
+                .foregroundColor(.red)
+                .alert(isPresented: $showAlert) {
+                    Alert(title: Text("notifications-allow"), message: Text("notifications-allow-message"), primaryButton: .default(Text("settings-open"), action: {
+                        if let url = URL(string: UIApplication.openSettingsURLString) {
+                            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                        }
+                    }), secondaryButton: .cancel())
+                }
+                
+            }
+            
+        }.environmentObject(notificationStatus)
+    }
+
+
 }
 
 extension ReminderPicker {
