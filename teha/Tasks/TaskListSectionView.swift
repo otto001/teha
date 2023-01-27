51f4d7ab440b963dbb638c7f5645469d2a12e7b8
Matteo Ludwig <ludwig@silpion.de>
2023-01-27 12:31:41 +0100

    improved TaskListSectionView by making the no-deadline section collapsible

--- teha/Tasks/TaskListSectionView.swift
+++ teha/Tasks/TaskListSectionView.swift
@@ -32,8 +32,6 @@ fileprivate func formatWeekOfYear(year: Int, weekOfYear: Int) -> String {
 
 fileprivate let yearMonthParseFormatter: DateFormatter = {
     let dateFormatter = DateFormatter()
-
-    //dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
     dateFormatter.dateFormat = "yyyy-MM"
     return dateFormatter
 }()
@@ -62,6 +60,8 @@ struct TaskListSectionView: View {
     
     let deleteAction: (_ task: THTask) -> Void
     
+    @State private var collapsed: Bool = false
+    
     init(_ section: SectionedFetchResults<String, THTask>.Element, deleteAction: @escaping (_ task: THTask) -> Void) {
         self.section = section
         self.deleteAction = deleteAction
@@ -71,8 +71,6 @@ struct TaskListSectionView: View {
         let id = section.id
         guard id != "none" else { return "No Deadline" }
         let year = Int(id.substring(start: 0, end: 4))!
-        
-        let currentYear = Calendar.current.component(.year, from: .now)
         
         if id.count >= 8 && id.substring(start: 4, end: 7) == "-CW" {
             // id is of format [year]-CW[weekOfYear]
@@ -93,28 +91,39 @@ struct TaskListSectionView: View {
     
     var body: some View {
         Section {
-            ForEach(section) { task in
-                TaskRowView(task: task)
-                    .swipeActions(edge: .trailing) {
-                        Button() {
-                            deleteAction(task)
-                        } label: {
-                            Label("delete", systemImage: "trash")
-                        }
-                        .tint(Color.red)
+            if section.id == "none" {
+                Button {
+                    withAnimation {
+                        collapsed.toggle()
                     }
-                    .disabled(editMode?.wrappedValue == .active)
-                    .id(task.id)
-                    .tag(task.objectID)
+                } label: {
+                    HStack {
+                        Text("\(section.count) Tasks without Deadline")
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .rotationEffect(Angle(degrees:collapsed ? 0 : 90))
+                    }
+                }.disabled((editMode?.wrappedValue.isEditing ?? false) && !collapsed)
             }
+            if section.id != "none" || !collapsed {
+                ForEach(section) { task in
+                    TaskRowView(task: task)
+                        .swipeActions(edge: .trailing) {
+                            Button {
+                                deleteAction(task)
+                            } label: {
+                                Label("delete", systemImage: "trash")
+                            }
+                            .tint(Color.red)
+                        }
+                        .disabled(editMode?.wrappedValue == .active)
+                        .id(task.id)
+                        .tag(task.objectID)
+                }
+            }
+            
         } header: {
             Text(sectionTitle)
         }
     }
 }
-
-//struct TaskListSectionView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        TaskListSectionView()
-//    }
-//}
