026461a5f8c42519b8d1a4148ba903fe6e8e0133
margo1999 <jette.oberlaender@rwth-aachen.de>
2023-01-29 19:16:29 +0100

    removed force-unwrapping in today() and thisWeek() and added documentation

--- teha/Tasks/TasksFilterViewModel.swift
+++ teha/Tasks/TasksFilterViewModel.swift
@@ -103,26 +103,36 @@ class TasksFilterViewModel: ObservableObject {
         return fetchRequest
     }
     
+    /**
+     Returns the date interval for today, starting from 00:00 AM  and ending on 11:59 PM.
+     
+     - Returns: A `DateInterval` object representing today. If a created optional is `nil`, the function will return a `deafaultInterval` which will return a `DateInterval` object where start and end time is equal to the current time.
+    */
     private func today() -> DateInterval {
+        let defaultDate = DateInterval()
+        
         let calendar = Calendar.current
         let now = Date()
-        let startOfDay = calendar.date(bySettingHour: 00, minute: 00, second: 00, of: now)!
-        print(startOfDay)
-        let endOfDay = calendar.date(bySettingHour: 23, minute: 59, second: 59, of: now)!
-        print(endOfDay)
+        guard let startOfDay = calendar.date(bySettingHour: 00, minute: 00, second: 00, of: now) else { return defaultDate }
+        guard let endOfDay = calendar.date(bySettingHour: 23, minute: 59, second: 59, of: now) else { return defaultDate }
         return DateInterval(start: startOfDay, end: endOfDay)
     }
     
+    /**
+     Returns the date interval for the current week, starting from Monday 00:00 AM  and ending on Sunday 11:59 PM.
+     
+     - Returns: A `DateInterval` object representing the current week. If a created optional is `nil`, the function will return a `deafaultInterval` which will return a `DateInterval` object where start and end time is equal to the current time.
+    */
     private func thisWeek() -> DateInterval {
-        let calendar = Calendar.current
+        let defaultDate = DateInterval()
+        
+        var calendar = Calendar.current
         let now = Date()
-        var startOfWeek = calendar.date(from: calendar.dateComponents([.yearForWeekOfYear, .weekOfYear], from: now))!
-        startOfWeek = calendar.date(byAdding: .weekOfYear, value: -1, to: startOfWeek)!
-        startOfWeek = calendar.date(byAdding: .day, value: 1, to: startOfWeek)!
-        print(startOfWeek)
-        var endOfWeek = calendar.date(byAdding: .day, value: 6, to: startOfWeek)!
-        endOfWeek = calendar.date(bySettingHour: 23, minute: 59, second: 59, of: endOfWeek)!
-        print(endOfWeek)
+        guard var startOfWeek = calendar.date(from: calendar.dateComponents([.yearForWeekOfYear, .weekOfYear], from: now)) else { return defaultDate }
+        guard var startOfWeek = calendar.date(byAdding: .weekOfYear, value: -1, to: startOfWeek) else { return defaultDate }
+        guard var startOfWeek = calendar.date(byAdding: .day, value: 1, to: startOfWeek) else { return defaultDate }
+        guard var endOfWeek = calendar.date(byAdding: .day, value: 6, to: startOfWeek) else { return defaultDate }
+        guard var endOfWeek = calendar.date(bySettingHour: 23, minute: 59, second: 59, of: endOfWeek) else { return defaultDate }
         return DateInterval(start: startOfWeek, end: endOfWeek)
     }
     
