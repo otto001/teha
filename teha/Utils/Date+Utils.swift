97fff9ab8275525bb40a6fcc55e1a87446be7296
Matteo Ludwig <ludwig@silpion.de>
2023-01-29 15:43:27 +0100

    Added some Comments

--- teha/Utils/Date+Utils.swift
+++ teha/Utils/Date+Utils.swift
@@ -8,10 +8,12 @@
 import Foundation
 
 extension Date {
+    /// Add a TimeInterval to the Date
     static func +(lhs: Date, rhs: TimeInterval) -> Date {
         return Date(timeIntervalSinceReferenceDate: lhs.timeIntervalSinceReferenceDate + rhs)
     }
     
+    /// Subtract a TimeInterval from the Date
     static func -(lhs: Date, rhs: TimeInterval) -> Date {
         return Date(timeIntervalSinceReferenceDate: lhs.timeIntervalSinceReferenceDate - rhs)
     }
